6ecd9e2fb70bca43fedb03b82e72a852026ad087
venkatesh-na <naikvenkatesh723@gmail.com>
2021-10-18 14:09:24 +0530

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Form from "./componets/Form"
 import Movies from "./componets/Movies"
 import Detail from "./componets/Detail";
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <Router>
